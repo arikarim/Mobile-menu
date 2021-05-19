81760797f725eb9f68a171a4e84a12599874cee9
arikarim <arikarim146@gmail.com>
2021-05-19 22:10:50 +0300

    Navlinks hide on button click

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const menuButton = document.getElementById('menuu');
 const mobileNav = document.getElementById('mobile-navv');
 const desctopNav = document.getElementById('desktop-nav');
 
-// mobileNav.style.display = 'none'
 menuButton.addEventListener('click', e => {
  
   mobileNav.style.display =  'block' 
@@ -11,10 +10,30 @@ menuButton.addEventListener('click', e => {
 })
 
 const navLink1 = document.getElementById('nav-v1');
-const navLink2 = document.getElementById('nav-v1');
-const navLink3 = document.getElementById('nav-v1');
+const navLink2 = document.getElementById('nav-v2');
+const navLink3 = document.getElementById('nav-v3');
 
 navLink1.addEventListener('click', () => {
+  menuButton.style.display = 'block';
+  mobileNav.style.display = 'none';
+  desctopNav.style.display =  'none' 
+})
+
+navLink2.addEventListener('click', () => {
+  menuButton.style.display = 'block';
+  mobileNav.style.display = 'none';
+  desctopNav.style.display =  'none' 
+})
+
+navLink3.addEventListener('click', () => {
+  menuButton.style.display = 'block';
+  mobileNav.style.display = 'none';
+  desctopNav.style.display =  'none' 
+})
+
+const close1 = document.getElementById('span1');
+
+close1.addEventListener('click', () => {
   menuButton.style.display = 'block';
   mobileNav.style.display = 'none';
   desctopNav.style.display =  'none' 

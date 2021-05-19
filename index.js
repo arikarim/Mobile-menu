3cfcac146b6129037aac49ef9fac59777d22efec
arikarim <arikarim146@gmail.com>
2021-05-19 22:03:46 +0300

    Show and hide mobile nav onClick

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const menuButton = document.getElementById('menuu');
 const mobileNav = document.getElementById('mobile-navv');
-mobileNav.style.display = 'none'
+const desctopNav = document.getElementById('desktop-nav');
+
+// mobileNav.style.display = 'none'
 menuButton.addEventListener('click', e => {
  
-  mobileNav.style.display = 'none' ? 'block' : 'none'
+  mobileNav.style.display =  'block' 
+  desctopNav.style.display =  'block' 
   menuButton.style.display = 'none' 
+})
+
+const navLink1 = document.getElementById('nav-v1');
+const navLink2 = document.getElementById('nav-v1');
+const navLink3 = document.getElementById('nav-v1');
+
+navLink1.addEventListener('click', () => {
+  menuButton.style.display = 'block';
+  mobileNav.style.display = 'none';
+  desctopNav.style.display =  'none' 
 })

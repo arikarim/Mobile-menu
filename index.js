68ff3fc9359384bcae4702fdb71639dae56cd2b0
arikarim <arikarim146@gmail.com>
2021-05-19 22:16:12 +0300

    Add linters

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const menuButton = document.getElementById('menuu');
 const mobileNav = document.getElementById('mobile-navv');
 const desctopNav = document.getElementById('desktop-nav');
 
-menuButton.addEventListener('click', e => {
- 
-  mobileNav.style.display =  'block' 
-  desctopNav.style.display =  'block' 
-  menuButton.style.display = 'none' 
-})
+menuButton.addEventListener('click', () => {
+  mobileNav.style.display = 'block';
+  desctopNav.style.display = 'block';
+  menuButton.style.display = 'none';
+});
 
 const navLink1 = document.getElementById('nav-v1');
 const navLink2 = document.getElementById('nav-v2');
@@ -16,25 +15,25 @@ const navLink3 = document.getElementById('nav-v3');
 navLink1.addEventListener('click', () => {
   menuButton.style.display = 'block';
   mobileNav.style.display = 'none';
-  desctopNav.style.display =  'none' 
-})
+  desctopNav.style.display = 'none';
+});
 
 navLink2.addEventListener('click', () => {
   menuButton.style.display = 'block';
   mobileNav.style.display = 'none';
-  desctopNav.style.display =  'none' 
-})
+  desctopNav.style.display = 'none';
+});
 
 navLink3.addEventListener('click', () => {
   menuButton.style.display = 'block';
   mobileNav.style.display = 'none';
-  desctopNav.style.display =  'none' 
-})
+  desctopNav.style.display = 'none';
+});
 
 const close1 = document.getElementById('span1');
 
 close1.addEventListener('click', () => {
   menuButton.style.display = 'block';
   mobileNav.style.display = 'none';
-  desctopNav.style.display =  'none' 
-})
+  desctopNav.style.display = 'none';
+});

3671cc4d2ef05167c5127f65c4883a03f9e4a859
arikarim <arikarim146@gmail.com>
2021-05-20 22:27:42 +0300

    Add close button to mobile nav

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const menuButton = document.getElementById('menuu');
 const mobileNav = document.getElementById('mobile-navv');
 const desctopNav = document.getElementById('desktop-nav');
-
+const logo = document.getElementById('logo')
 menuButton.addEventListener('click', () => {
   mobileNav.style.display = 'block';
   desctopNav.style.display = 'block';
   menuButton.style.display = 'none';
+  logo.style.display = 'none';
 });
 
 const navLink1 = document.getElementById('nav-v1');
@@ -16,18 +17,23 @@ navLink1.addEventListener('click', () => {
   menuButton.style.display = 'block';
   mobileNav.style.display = 'none';
   desctopNav.style.display = 'none';
+  logo.style.display = 'block';
 });
 
 navLink2.addEventListener('click', () => {
   menuButton.style.display = 'block';
   mobileNav.style.display = 'none';
   desctopNav.style.display = 'none';
+  logo.style.display = 'block';
+
 });
 
 navLink3.addEventListener('click', () => {
   menuButton.style.display = 'block';
   mobileNav.style.display = 'none';
   desctopNav.style.display = 'none';
+  logo.style.display = 'block';
+
 });
 
 const close1 = document.getElementById('span1');
@@ -36,4 +42,5 @@ close1.addEventListener('click', () => {
   menuButton.style.display = 'block';
   mobileNav.style.display = 'none';
   desctopNav.style.display = 'none';
+  logo.style.display = 'block';
 });
